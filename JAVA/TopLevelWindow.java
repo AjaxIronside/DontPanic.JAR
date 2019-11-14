6b44995f75b677e6395e51035102383434d77d47
Al Such <alwsuch@gmail.com>
2019-11-13 22:59:56 -0500

    Main window improvements
    - Improved JFrame to show four basic content types:
    -- Image describing action on the screen
    --Descriptive text of action happening.
    --Text box for user inputs
    --Enter button to submit inputs.
    
    Next step is to get it all to function!

--- JAVA/TopLevelWindow.java
+++ JAVA/TopLevelWindow.java
@@ -5,14 +5,38 @@ public class TopLevelWindow {
    private static void createWindow() {
       //Create and set up the window.
       JFrame frame = new JFrame("The Hitchhiker's Guide To The Galaxy");
+      frame.setSize(700, 700);
+      
+      //Ensuring that closing the frame... closes the frame.
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      //A little icon for Windows users. 
+      frame.setIconImage(new ImageIcon("src/greenguy.png").getImage());
+      
+      //Display an image to describe the action happening.
+      ImageIcon picture = new ImageIcon("src/smiley.jpg");
+      JLabel picDisplay = new JLabel(picture);
+      picDisplay.setBounds(150, 100, 410, 310);
+      
+      //Text area. In future can maybe be used to describe what happens?
       JLabel textLabel = new JLabel("Don't Panic!",SwingConstants.CENTER);
-      textLabel.setPreferredSize(new Dimension(300, 100));
-      frame.getContentPane().add(textLabel, BorderLayout.CENTER);
-      //Display the window.
-      frame.setLocationRelativeTo(null);
-      frame.pack();
+      textLabel.setBounds(250, 450, 200, 30);
+      
+      //User input area, where user will type responses.
+      JTextField userInput = new JTextField("Enter a response");
+      userInput.setBounds(250, 500, 200, 30);
+      
+      //Button, to submit commands.
+      JButton enterButt = new JButton("Enter");
+      enterButt.setBounds(300, 550, 100, 30);
+      
+      frame.add(picDisplay);
+      frame.add(textLabel);
+      frame.add(userInput);
+      frame.add(enterButt);
+      frame.setLayout(null);
       frame.setVisible(true);
+      
    }
    public static void main(String[] args) {
       createWindow();
